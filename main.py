9e9b120ccbc46870f4ce71b23b7329cb98bef62b
kerneltestpilot <oskardah@hotmail.com>
2026-04-24 14:25:55 +0200

    Adeded machine learning

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from userClass import User
 
 users = User.load_users()
 
-user = User.create_user(1971, "Female", "Vasteras", False, "Hann inte presentera")
+user = User.create_user(1957, "Female", "Jönköping", False, "Inte tid")
 
 users.append(user)
 
